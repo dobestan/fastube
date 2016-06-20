cd7f2ae4446f64e192b46be3eeb629cf61472f90
Suchan An <me@ansuchan.com>
2016-06-20 16:17:10 +0900

    init base templates: add layout, and partials templates

--- fastube/fastube/settings/partials/base.py
+++ fastube/fastube/settings/partials/base.py
@@ -58,7 +58,9 @@ ROOT_URLCONF = 'fastube.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            os.path.join(BASE_DIR, "fastube", "templates"),
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
