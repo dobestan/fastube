ae0c344cc549edf11704422e48c1a6ff6b57aa23
Suchan An <me@ansuchan.com>
2016-06-21 17:33:26 +0900

    update settings: add kakao social login settings

--- fastube/fastube/settings/partials/base.py
+++ fastube/fastube/settings/partials/base.py
@@ -167,12 +167,16 @@ LOGOUT_SUCCESS_MESSAGE = "성공적으로 로그아웃 되었습니다"
 
 AUTHENTICATION_BACKENDS = [
     "social.backends.facebook.FacebookOAuth2",
+    "social.backends.kakao.KakaoOAuth2",
 
     "django.contrib.auth.backends.ModelBackend",
 ]
 
 SOCIAL_AUTH_FACEBOOK_KEY = "237373486648488"
 SOCIAL_AUTH_FACEBOOK_SECRET = "9a31e73646f72c20d061a81a9563d139"
+
+SOCIAL_AUTH_KAKAO_KEY = "20fd3acbcf04cbb3d46839f06e3f7f6d"
+SOCIAL_AUTH_KAKAO_SECRET = "3cd433fd15f6c112c36181e5a9380372"
 
 SOCIAL_AUTH_PIPELINE = (
     'social.pipeline.social_auth.social_details',
