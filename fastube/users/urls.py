1abb89685339c97e2bf9dc66f2cf9b4d23e886c9
Suchan An <me@ansuchan.com>
2016-06-21 16:53:38 +0900

    fix urls.py: explicit end keyword on users urls

--- fastube/users/urls.py
+++ fastube/users/urls.py
@@ -4,7 +4,7 @@ from users.views import *
 
 
 urlpatterns = [
-    url(r'^signup/', SignupView.as_view(), name="signup"),
-    url(r'^login/', LoginView.as_view(), name="login"),
-    url(r'^logout/', LogoutView.as_view(), name="logout"),
+    url(r'^signup/$', SignupView.as_view(), name="signup"),
+    url(r'^login/$', LoginView.as_view(), name="login"),
+    url(r'^logout/$', LogoutView.as_view(), name="logout"),
 ]
