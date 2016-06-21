69b5acbefd0666de8abb82e99d8dbdeeddd88847
Suchan An <me@ansuchan.com>
2016-06-21 14:38:32 +0900

    update SignupView: create user with input values without any validations, and flash messages

--- fastube/users/views/signup.py
+++ fastube/users/views/signup.py
@@ -1,5 +1,7 @@
+from django.contrib.auth import get_user_model
+from django.core.urlresolvers import reverse
+from django.shortcuts import render, redirect
 from django.views.generic import View
-from django.shortcuts import render
 
 
 class SignupView(View):
@@ -10,3 +12,20 @@ class SignupView(View):
             "users/signup.html",
             context={},
         )
+
+    def post(self, request, *args, **kwargs):
+        username = request.POST.get("username")
+        password = request.POST.get("password")
+        phonenumber = request.POST.get("phonenumber")
+
+        # TODO: validations ( username )
+
+        user = get_user_model().objects.create_user(
+            username=username,
+            password=password,
+            phonenumber=phonenumber,
+        )
+
+        # TODO: flash messages ( success, error messages )
+
+        return redirect(reverse("login"))
