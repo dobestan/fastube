e47041c643f85265651d9408e805bce2ec7f0318
Suchan An <me@ansuchan.com>
2016-06-22 13:44:58 +0900

    update Post model: add get_youtube_original_url method

--- fastube/posts/models/post.py
+++ fastube/posts/models/post.py
@@ -23,7 +23,11 @@ class Post(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
-    # TODO: youtube original url
+    def get_youtube_original_url(self):
+        return "https://www.youtube.com/watch?v={video_id}".format(
+            video_id=self.video_id,
+        )
+
     # TODO: youtube embed source code
 
     def __str__(self):
