62f8837266146ea798db9db5ed88704fa49b2990
Suchan An <me@ansuchan.com>
2016-06-27 15:53:03 +0900

    update posts.js: alert comments content on comments create form submit

--- fastube/fastube/static/js/posts.js
+++ fastube/fastube/static/js/posts.js
@@ -1,3 +1,13 @@
 $(document).ready(function() {
-  alert("hello world");
+  var commentsSectionElement = $("#comments-section");
+  var commentsCreateFormElement = $(commentsSectionElement).find("form");
+  var commentsCreateFormInputContentElement = $(commentsCreateFormElement).find("input[name='content']");
+
+  commentsCreateFormElement.submit(function() {
+    var content = $(commentsCreateFormInputContentElement).val();
+    alert(content);
+    $(commentsCreateFormInputContentElement).val("");
+
+    return false;
+  });
 });
