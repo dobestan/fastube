086f5f82d2377738e9f0a93692e6902023096166
Suchan An <me@ansuchan.com>
2016-06-27 13:39:28 +0900

    refactor api urls

--- fastube/fastube/urls/api.py
+++ fastube/fastube/urls/api.py
@@ -1,8 +1,10 @@
-from django.conf.urls import url
+from django.conf.urls import url, include
 
 from posts.api import *
 
 
 urlpatterns = [
-    url(r'^posts/$', PostListAPIView.as_view()),
+    url(r'^posts/', include([
+        url(r'^$', PostListAPIView.as_view(), name="list"),
+    ], namespace="posts")),
 ]
