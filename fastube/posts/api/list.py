30cf520e3f99e8587cdba1c2b2d4d9b747b9a128
Suchan An <me@ansuchan.com>
2016-06-27 13:43:46 +0900

    update PostListAPIView: get all posts and serialize data to json

--- fastube/posts/api/list.py
+++ fastube/posts/api/list.py
@@ -1,8 +1,26 @@
+import json
+
 from django.views.generic import View
 from django.http.response import HttpResponse
+
+from posts.models import Post
 
 
 class PostListAPIView(View):
 
     def get(self, request, *args, **kwargs):
-        return HttpResponse("posts:list")
+
+        data = [
+            {
+                "title": post.title,
+                "content": post.content,
+                "youtube_original_url": post.youtube_original_url,
+            }
+            for post
+            in Post.objects.all()
+        ]
+
+        return HttpResponse(
+            json.dumps(data),
+            content_type="application/json",
+        )
