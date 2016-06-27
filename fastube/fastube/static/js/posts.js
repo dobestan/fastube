fd45ecc597c707ac7fb31490134366cdc413c578
Suchan An <me@ansuchan.com>
2016-06-27 16:17:12 +0900

    update posts.js: render comments count, and list via ajax request

--- fastube/fastube/static/js/posts.js
+++ fastube/fastube/static/js/posts.js
@@ -1,7 +1,36 @@
 $(document).ready(function() {
   var commentsSectionElement = $("#comments-section");
+
+  var commentsCountElement = $(commentsSectionElement).find("#comments-count");
+
+  var commentsListElement = $(commentsSectionElement).find("ul");
+
   var commentsCreateFormElement = $(commentsSectionElement).find("form");
   var commentsCreateFormInputContentElement = $(commentsCreateFormElement).find("input[name='content']");
+
+  var postSlug = $(commentsSectionElement).data("post-slug");
+  var commentsAPIUrl = "/api/posts/" + postSlug + "/comments/";
+
+  $.ajax({
+    url: commentsAPIUrl,
+    type: "GET",
+    success: function(data) {
+      var commentsCount = data.length;
+      $(commentsCountElement).html(commentsCount);
+
+      data.forEach(function(comment) {
+        var commentUsername= comment.username;
+        var commentContent = comment.content;
+
+        var commentData = commentUsername + ": " + commentContent;
+
+        var commentListElement = $("<li>").text(commentData);
+        $(commentsListElement).append(commentListElement);
+      });
+    },
+    error: function(data) {
+    }
+  });
 
   commentsCreateFormElement.submit(function() {
     var content = $(commentsCreateFormInputContentElement).val();
