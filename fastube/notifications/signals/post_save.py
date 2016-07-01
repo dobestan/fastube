fb30540a9366a4cb83ad4cf17ca55b3a03037583
Suchan An <me@ansuchan.com>
2016-07-01 15:40:09 +0900

    init post_save_notification signal

---
+++ fastube/notifications/signals/post_save.py
@@ -0,0 +1,11 @@
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+
+from notifications.models import SMSNotification, SlackNotification
+
+
+@receiver(post_save, sender=SMSNotification)
+@receiver(post_save, sender=SlackNotification)
+def post_save_notification(sender, instance, created, **kwargs):
+    if created:
+        pass
